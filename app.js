f7fab82a7337b2955e9881529e3fda4d8dcd50d4
Barzi Jaf <barzidev@gmail.com>
2023-08-19 21:45:19 +0200

    works up to 999999999

--- app.js
+++ app.js
@@ -1,36 +1,60 @@
 let inputHTML = document.querySelector('input');
 let outputHTML = document.querySelector('p');
 
-inputHTML.addEventListener('input', convertToText)
+const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const tens = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-function convertToText() {
-    let ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
-                 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+inputHTML.addEventListener('input', () => {
+    outputHTML.innerHTML = "Output: " + convertToText(inputHTML.value);
+})
 
-    let tens = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+function convertToText(number) {
 
-    let firstDigitOfInput, secondDigitOfInput, thirdDigitOfInput;
+    if(number == 0) 
+        return "zero";
 
-    if(inputHTML.value < 0) {
-        outputHTML.textContent = `Output: negative ${ones[inputHTML.value.slice(1)]}`;
-    }
+    else
+        return convertMillions(number);
     
-    else if(inputHTML.value < 20) {
-        outputHTML.textContent = `Output: ${ones[inputHTML.value]}`;
-    }
+    function convertTens(number) {
+       if(number < 10)
+            return ones[number];
 
-    else if(inputHTML.value.length == 2) {
-        firstDigitOfInput = inputHTML.value.slice(0,1);
-        secondDigitOfInput = inputHTML.value.slice(1);
-        outputHTML.textContent = `Output: ${tens[firstDigitOfInput]}${inputHTML.value % 10 ? '-'+ones[secondDigitOfInput] : ''}`;
-    }
+        else if(number >= 10 && number < 20) 
+            return teens[number - 10];
 
-    else if(inputHTML.value.length == 3) {
-        firstDigitOfInput = inputHTML.value.slice(0,1);
-        secondDigitOfInput = inputHTML.value.slice(1,2);
-        thirdDigitOfInput = inputHTML.value.slice(2);
-        outputHTML.textContent = `Output: ${ones[firstDigitOfInput] + ' hundred ' + tens[secondDigitOfInput]}${inputHTML.value.slice(1) % 10 ? '-'+ones[thirdDigitOfInput] : ''} `;
-        console.log(thirdDigitOfInput);
+        else if(number >= 20 && number <= 99) 
+            if(number % 10 != 0)
+                return tens[Math.floor(number / 10)] + '-' + ones[number % 10];
+            else
+                return tens[Math.floor(number / 10)];
+        else 
+            return '';
+
+    }
+    function convertHundreds(number) {
+        if(number > 99)
+            return ones[Math.floor(number / 100)] + ' hundred ' + convertTens(number%100);
+        else 
+            return convertTens(number)
+    }
+    function convertThousands(number) {
+        if(number > 999 && number <= 99999) 
+            return convertTens(Math.floor(number/1000)) + ' thousand ' + convertHundreds(number % 1000);
+        else if(number > 99999 && number <=999999) 
+            return convertHundreds(Math.floor(number/1000)) + ' thousand ' + convertHundreds(number % 1000);
+        else 
+            return convertHundreds(number);
+    }
+    function convertMillions() {
+    // && number <= 99999999
+        if(number > 999999 && number <= 99999999) 
+            return convertTens(Math.floor(number/1000000)) + ' million ' + convertThousands(Math.floor(number %1000000));
+        else if(number > 99999999 && number <= 999999999) 
+            return convertHundreds(Math.floor(number/1000000)) + ' million ' + convertThousands(Math.floor(number %1000000));
+        else 
+            return convertThousands(number);
     }
 
 }
